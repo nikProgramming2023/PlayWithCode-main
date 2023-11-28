ba3b81522445b9e563fcfa8f06fa5d406e91ad01
Niklas Koenig <nikkoe05@gmail.com>
2023-11-28 12:50:44 -0600

    Now Creating Arrays

---
+++ README.md
@@ -0,0 +1,3 @@
+# JavaScript All-in-One For Dummies
+
+I'm learning JavaScript, react, vue, Svelte, and node from JavaScript All-in-One For Dummies by Chris Minnick
---
+++ index.js
@@ -0,0 +1,8 @@
+const colors = ['Red', 'Blue', 'Green'];
+
+//Getting into arrays
+
+new Array();
+const Months = new Array('January', 'Febuary', 'March', 'April', 'May');
+
+const breakfast = ['Eggs', 'Bacon', 'Pancakes']
