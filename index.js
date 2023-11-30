bae9ee5192ee5d8b0c4ab0f07dc991c986ea0d8e
Niklas Koenig <nikkoe05@gmail.com>
2023-11-30 11:24:50 -0600

    End of Chapter 6

--- index.js
+++ index.js
@@ -1,8 +1,78 @@
-const colors = ['Red', 'Blue', 'Green'];
+//const colors = ["Red", "Blue", "Green"];
 
 //Getting into arrays
 
-new Array();
-const Months = new Array('January', 'Febuary', 'March', 'April', 'May');
+//new Array();
+//const Months = new Array("January", "Febuary", "March", "April", "May");
 
-const breakfast = ['Eggs', 'Bacon', 'Pancakes']
+//const breakfast = ["Eggs", "Bacon", "Pancakes"];
+
+//const myArray = [43, 199, 123];
+//let sum = 0;
+//for (let i = 0; i < myArray.length - 1; ++i) {
+//sum += myArray[i];
+//}
+//console.log(`The sum is ${sum}`);
+
+//const ingrediants = [
+//"Eggs",
+//   //"Milk",
+//   "Cheese",
+//   "Garlic",
+//   "Onion",
+//   "Kale",
+//   "Salt",
+//   "Pepper",
+//];
+
+//CallBack Functions
+//function squareIt(num) {
+//return num * num;
+//};
+//console.log(squareIt);
+
+// const ingrediants = [
+// "Eggs",
+// "Milk",
+// "Cheese",
+// "Garlic",
+// "Onion",
+// "Kale",
+// "Salt",
+// "Pepper",
+// ];
+// let listItems = ingrediants
+// .map((singleIngrediants) => `<li>${singleIngrediants}</li>`)
+// .join(``);
+// document.getElementById(`ingrediants`).innerHTML = listItems;
+// How to filter arrays
+
+// const animalNames = [
+// "Aardvark",
+// "Alligator",
+// "Alpaca",
+// "Bear",
+// "Beaver",
+// "Cat",
+// "Dog",
+// "Elephant",
+// ];
+// const animalsStratingWithA = animalNames.filter((animal) =>
+// animal.startsWith("A")
+// );
+
+// document.getElementById("animal-names").innerHTML = animalNames;
+// document.getElementById("animal-names-start-with-a").innerHTML =
+// animalsStratingWithA;
+
+// animalNames.map((allAnimals) => `<li>${allAnimals}</li>`).join("");
+
+//Spreading Arrays
+
+// const firstArray = ["a", "b", "c"];
+// const secongArray = [...firstArray, "d"];
+
+// const fruits = ["Apple", "Bannana", "Cantelope"];
+// const fruitsCopy = [...fruits];
+// fruitsCopy.push("Dragon Fruit");
+// conlsole.log(fruits, fruitsCopy);
